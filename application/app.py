6945f5a5472a0aa8c676ad4900010cc497c2d330
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2025-02-25 22:10:53 +0530

    Update app.py

--- application/app.py
+++ application/app.py
@@ -44,6 +44,8 @@ def predict_image_class(img_path):
         predictions = model(img_tensor)
     predicted_class = torch.argmax(predictions, dim=1).item()
     confidence = torch.softmax(predictions, dim=1)[0, predicted_class].item() * 100
+    if confidence < 70:
+        return "OCT_UNKNOWN", confidence
     return predicted_class, confidence
 
 # Step 5: Map predicted class to label
@@ -110,7 +112,7 @@ def authenticate():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    if username == "admin" and password == "password":
+    if username == "admin" and password == "123":
         return jsonify({"message": "Login Successful"})
     else:
         return jsonify({"message": "Invalid Credentials"}), 401
